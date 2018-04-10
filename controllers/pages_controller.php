a2bd6d80cc94f599cb1b8e39fe38427acc74b15c
Tiago Lisboa <tiago.caio.ol@gmail.com>
2018-04-09 21:47:27 -0300

    aaa

--- controllers/pages_controller.php
+++ controllers/pages_controller.php
@@ -46,7 +46,7 @@
             $corpo .= "Email: " . $email . "\n";
             $corpo .= "Comentários: " . $message . "\n";
             
-            $email_to = 'trollwisard@gmail.com';
+            $email_to = 'administração@dentalclean.com.br';
             //não esqueça de substituir este email pelo seu.
             
             $status = mail($email_to, $subject, $corpo, $headers);
