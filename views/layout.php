e524ab5c2883de33cb327489b28ddbbb31495ae6
Tiago Lisboa <tiago.caio.ol@gmail.com>
2018-04-12 23:05:44 -0300

    aaa

--- views/layout.php
+++ views/layout.php
@@ -6,13 +6,10 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>ServDent</title>
     <link rel="icon" href="<?= __BASE_URI__ ?>views/assets/imgs/logo.png">
-    <link rel="stylesheet"
-        href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-        crossorigin="anonymous"
-    >
+    <link rel="stylesheet" href="<?= __BASE_URI__ ?>views/assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="<?= __BASE_URI__ ?>views/assets/css/style.css">
     <script src="<?= __BASE_URI__ ?>views/assets/js/jquery-3.3.1.min.js"></script>
+    <script src="<?= __BASE_URI__ ?>views/assets/js/bootstrap.min.js"></script>
     <script src="<?= __BASE_URI__ ?>views/assets/js/moment-with-locales.js"></script>
     <script type="text/javascript" src="<?= __BASE_URI__ ?>views/assets/js/tempusdominus-bootstrap-4.min.js"></script>
     <link rel="stylesheet" href="<?= __BASE_URI__ ?>views/assets/css/tempusdominus-bootstrap-4.min.css" />
@@ -28,21 +25,36 @@
                     <img src="<?= __BASE_URI__ ?>views/assets/imgs/logo.png" alt="logo ServDent">
                 </a>
             </figure>
-            <ul class="navbar-nav mr-auto col-sm-3 offset-sm-4">
-                <li class="nav-item"><a href="<?= __BASE_URI__ ?>" id="link-inicio" class="nav-link">Início</a></li>
-                <li class="nav-item"><a href="<?= __BASE_URI__ ?>?controller=pages&action=servicos" id="link-servico" class="nav-link">Serviços</a></li>
-                <li class="nav-item"><a href="<?= __BASE_URI__ ?>?controller=login&action=index"  id="link-login" class="nav-link">
-                    <?= isset($_SESSION['usuario']) ? 'Conta' : 'Login' ?>
-                </a></li>
-                <li class="nav-item"><a href="<?= __BASE_URI__ ?>?controller=pages&action=contato" class="nav-link"  id="link-contato">Contato</a></li>
-<?php if (isset($_SESSION['usuario'])) { ?><li class="nav-item"><a href="<?= __BASE_URI__ ?>?controller=login&action=logout" class="nav-link">Logout</a></li><?php } ?>
-            </ul>
-            <div class="col-sm-3">
+            <div class="col-sm-3 offset-sm-2">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item"><a href="<?= __BASE_URI__ ?>" id="link-inicio" class="nav-link">Início</a></li>
+                    <li class="nav-item"><a href="<?= __BASE_URI__ ?>?controller=pages&action=servicos" id="link-servico" class="nav-link">Serviços</a></li>
+                    <li class="nav-item"><a href="<?= __BASE_URI__ ?>?controller=login&action=index"  id="link-login" class="nav-link">
+                        <?= isset($_SESSION['usuario']) ? 'Conta' : 'Login' ?>
+                    </a></li>
+                    <li class="nav-item"><a href="<?= __BASE_URI__ ?>?controller=pages&action=contato" class="nav-link"  id="link-contato">Contato</a></li>
+                </ul>
+            </div>
+            <div class="col-sm-3 offset-sm-1">
                 <form class="">
                     <input class="search-input" type="search" placeholder="O que você procura?" aria-label="Search">
                     <button class="search-button" type="submit"><img src="<?= __BASE_URI__ ?>views/assets/imgs/lupa.png" alt="lupa"></button>
                 </form>
             </div>
+            <?php if (isset($_SESSION['usuario'])) { ?>
+                <div class="col-sm-1">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item dropdown">
+                            <a href="#" class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <?= $_SESSION['usuario']->usuario ?>
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a href="<?= __BASE_URI__ ?>?controller=login&action=logout" class="dropdown-item">Logout</a>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            <?php } ?>
         </nav>
     </header>
 
